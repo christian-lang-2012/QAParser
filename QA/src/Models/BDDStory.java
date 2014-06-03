f9a53c7de102b522d5f82af8116b558d210a36f1
christianlang <christianlang@attask.com>
2014-06-03 16:09:18 -0600

    updated the models

---
+++ QA/src/Models/BDDStory.java
@@ -0,0 +1,49 @@
+package Models;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: christianlang
+ * Date: 6/2/14
+ */
+public class BDDStory {
+
+    private String theStory;
+    private ArrayList<BDDScenario> scenarios;
+
+    public void addScenario(BDDScenario bddScenario){
+        scenarios.add(bddScenario);
+    }
+
+    public String getTheStory() {
+        return theStory;
+    }
+
+    public void setTheStory(String theStory) {
+        this.theStory = theStory;
+    }
+
+    public ArrayList<BDDScenario> getScenarios() {
+        return scenarios;
+    }
+
+    public void setScenarios(ArrayList<BDDScenario> scenarios) {
+        this.scenarios = scenarios;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        if(theStory != null)
+            s += "Story:" + theStory + "\n";
+        else
+            s+= "Story: None:\n";
+
+        for(BDDScenario scenario : scenarios){
+            s += scenario.toString() + "\n";
+        }
+
+        return s;
+    }
+}
